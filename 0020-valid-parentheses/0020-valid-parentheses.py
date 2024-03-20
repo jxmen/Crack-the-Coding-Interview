280b996f7ac3f98cdfa2fbf7e9f8d9646011116f
Juyeong Park <sprnd645@gmail.com>
2024-03-20 23:56:56 +0900

    Time: 35 ms (57.78%), Space: 16.5 MB (75.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,29 +1,31 @@
 class Solution:
+    opens = set(list(['(', '[', '{']))
+    d = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    }
+    
     def isValid(self, s: str) -> bool:
         stack = []
-        opens = set(list(['(', '[', '{']))
         for c in list(s):
-            if c in opens:
+            if self.is_open(c):
                 stack.append(c)
-            else:
-                if len(stack) == 0:
-                    return False
-                popped = stack.pop()
-                if not self.validate(popped, c):
-                    return False
+                continue
+            
+            if len(stack) == 0:
+                return False
+            
+            popped = stack.pop()
+            if not self.validate(popped, c):
+                return False
         
         return len(stack) == 0
 
-    def validate(self, popped: str, c: str) -> bool:
-        # chante to dict
+    def is_open(self, c: str) -> bool:
+        return c in Solution.opens
 
-        if popped == '(' and c != ')':
-            return False
-        if popped == '[' and c != ']':
-            return False
-        if popped == '{' and c != '}':
-            return False
-        
-        return True
+    def validate(self, popped: str, c: str) -> bool:
+        return Solution.d[popped] == c
 
         
