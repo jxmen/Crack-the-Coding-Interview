40c8f7382bdfd011584f5409005d717a8edebc09
Juyeong Park <sprnd645@gmail.com>
2024-04-02 06:41:43 +0900

    Time: 4 ms (6.82%), Space: 46.4 MB (5.70%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -10,14 +10,14 @@ public class TreeNode {
         this.right = right;
     }
     
-    public List<Integer> getPreOrderValues() {
+    public List<Integer> getInOrderValues() {
         if (this == null) return List.of();
         
         List<Integer> integers = new ArrayList<>();
         
         // left
         List<Integer> leftValues = Optional.ofNullable(this.left)
-            .map(it -> it.getPreOrderValues())
+            .map(it -> it.getInOrderValues())
             .orElse(List.of());
         integers.addAll(leftValues);
 
@@ -26,7 +26,7 @@ public class TreeNode {
 
         // right
         List<Integer> rightValues = Optional.ofNullable(this.right)
-            .map(it -> it.getPreOrderValues())
+            .map(it -> it.getInOrderValues())
             .orElse(List.of());
         integers.addAll(rightValues);
         
@@ -40,8 +40,8 @@ class Solution {
     // k번째로 작은 숫자를 찾아 리턴하라. (k=1일 경우 가장 작은 숫자이다.)
     public int kthSmallest(TreeNode root, int k) {
         
-        // bst의 값들을 정렬된 array로 만들고 k-1 인덱스에 접근
-        List<Integer> preOrderIntegers = root.getPreOrderValues();
+        // bst의 값들을 정렬된 array로 만들고 k-1 인덱스에 접근 (시간복잡도: O(N))
+        List<Integer> preOrderIntegers = root.getInOrderValues();
         
         return preOrderIntegers.get(k-1);
     }
