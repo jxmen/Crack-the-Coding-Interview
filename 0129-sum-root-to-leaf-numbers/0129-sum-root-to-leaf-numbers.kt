5d76dfe796d76047a5ed39d76112770eac863000
Juyeong Park <sprnd645@gmail.com>
2024-04-03 16:27:58 +0900

    Time: 169 ms (8.82%), Space: 38.1 MB (5.04%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.kt
@@ -0,0 +1,42 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+fun TreeNode.hasChild(): Boolean {
+    return this.left != null || this.right != null
+}
+
+class Solution {
+    fun sumNumbers(root: TreeNode?): Int {
+        if (root == null) return 0
+        
+        // 자식의 문자열 목록들을 모두 받아와서 sum에 합치고 리턴한다.
+        
+        val list = getList(root)
+        return list.map { it -> it.toInt() }
+            .sum()
+        
+    }
+    
+    private fun getList(root: TreeNode?): List<String> {
+        if (root == null) return emptyList()
+        
+        if (!root.hasChild()) {
+            return listOf(root.`val`.toString())
+        }
+        
+        // 자식들의 리스트를 얻어서 합친다.
+        val left = getList(root.left)
+        val right = getList(root.right)
+        
+        return listOf(left, right).flatten()
+            .map { it -> root.`val`.toString() + it }
+    }
+}
