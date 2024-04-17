2cb6202571974c4c8f511c360cb5b514c00b8a9f
Juyeong Park <sprnd645@gmail.com>
2024-04-17 14:24:29 +0900

    Time: 0 ms (100.00%), Space: 47.4 MB (72.41%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        return countNodesHelp(root, 0);
+        return countNodeHelpOptimized(root);
     }
     
     private int countNodesHelp(TreeNode root, int count) {
@@ -23,5 +23,27 @@ class Solution {
         
         return 1 + countNodesHelp(root.left, count) 
             + countNodesHelp(root.right, count);
+    }
+    
+    // O(logN * logN) 으로 최적화한 버전
+    private int countNodeHelpOptimized(TreeNode root) {
+        if (root == null) return 0;
+        
+        // O(log N)
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);
+        
+        // 왼쪽이 완전 이진트리일 경우 leftDepth^2 + rightCount
+        if (leftDepth == rightDepth) {
+            return (int) Math.pow(2, leftDepth) + countNodeHelpOptimized(root.right);
+        } else {
+            return (int) Math.pow(2, rightDepth) + countNodeHelpOptimized(root.left);
+        }
+    }
+    
+    private int getDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        return 1 + getDepth(root.left);
     }
 }
