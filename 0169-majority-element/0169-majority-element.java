564a541010b6971df4140c7c649e130a9b194781
Juyeong Park <sprnd645@gmail.com>
2024-05-18 18:40:37 +0900

    Time: 2 ms (78.28%), Space: 52.9 MB (52.99%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,57 @@
+class Solution {
+
+    public int majorityElement(int[] nums) {
+      // majority element가 있기 때문에 이 알고리즘 풀이가 가능하다.
+      int majority = 0, count = 0;
+
+      for (int num : nums) {
+        /** count가 0이 되었다면 majority 후보가 바뀐다. */
+        if (count == 0) {
+          majority = num; 
+          count = 1;
+        } else if (num == majority) {
+          count++;
+        } else {
+          count--;
+        }
+      }
+
+      return majority;
+    }
+
+    // Follow-up: Could you solve the problem in linear time and in O(1) space?
+    public int majorityElement2(int[] nums) {
+      int majority = 0, count = 0;
+
+      for (int i = 1; i < nums.length; i++) {
+        if (count == 0) {
+          majority = nums[i];
+          count = 1;
+          continue;
+        }
+
+        if (nums[i] == majority) {
+          count++;
+          continue;
+        }
+
+        count--;
+        int iCount = findCount(nums, nums[i]); // findCount
+        if (iCount > count) {
+          majority = nums[i];
+          count = iCount;
+        }
+      }
+
+      return majority;
+    }
+
+    private int findCount(int[] nums, int target) {
+      int count = 0;
+      for (int num : nums) {
+        if (num == target) count++;
+      }
+
+      return count;
+    }
+}
