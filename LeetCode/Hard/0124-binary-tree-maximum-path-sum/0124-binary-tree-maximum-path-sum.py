bd40b4b39b2df152def3da1122465739600d23ba
Juyeong Park <sprnd645@gmail.com>
2024-06-02 06:19:14 +0900

    Time: 86 ms (5.23%), Space: 23.1 MB (7.05%) - LeetHub

--- LeetCode/Hard/0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ LeetCode/Hard/0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -7,45 +7,45 @@
 
 class Solution:
     cache = {}
-    
+
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        queue = deque([root])
-        pathSums = []
+        if not root:
+            return None
 
         # 모든 노드를 돌면서, 자신이 포함된 경로의 최대 합계를 구한다.
-        while len(queue):
-            queueLen = len(queue)
-            for i in range(queueLen):
-                node = queue.popleft()
-                left = self.maxPathSumFrom(node.left)
-                right = self.maxPathSumFrom(node.right)
+        left = self.maxPathSumFrom(root.left)
+        right = self.maxPathSumFrom(root.right)
 
-                # 현재 노드를 포함한 값 중 경로의 최대값을 추가한다.
-                currentNodeMaxPathSum = max(
-                    node.val,
-                    node.val + left,
-                    node.val + right,
-                    (node.val + left + right)
-                )
-                pathSums.append(currentNodeMaxPathSum)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+        # 현재 노드를 포함한 값 중 경로의 최대값을 추가한다.
+        maxPathSum = max(
+            root.val,
+            root.val + left,
+            root.val + right,
+            (root.val + left + right)
+        )
 
-        return max(pathSums)
-    
+        leftMaxPathSum = self.maxPathSum(root.left)
+        rightMaxPathSum = self.maxPathSum(root.right)
+        if leftMaxPathSum is None and rightMaxPathSum is None:
+            return maxPathSum
+        elif leftMaxPathSum is None:
+            return max(maxPathSum, rightMaxPathSum)
+        elif rightMaxPathSum is None:
+            return max(maxPathSum, leftMaxPathSum)
+        else:
+            return max(maxPathSum, leftMaxPathSum, rightMaxPathSum)
+
     # root를 포함한 경로의 최대값을 리턴한다.
     def maxPathSumFrom(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        
+
         if root in self.cache:
             return self.cache[root]
 
         if not root.left and not root.right:
             return root.val
-        
+
         if not root.right:
             left = self.maxPathSumFrom(root.left)
             return max(root.val, (root.val + left))
@@ -53,7 +53,7 @@ class Solution:
         if not root.left:
             right = self.maxPathSumFrom(root.right)
             return max(root.val, (root.val + right))
-        
+
         left = self.maxPathSumFrom(root.left)
         right = self.maxPathSumFrom(root.right)
 
