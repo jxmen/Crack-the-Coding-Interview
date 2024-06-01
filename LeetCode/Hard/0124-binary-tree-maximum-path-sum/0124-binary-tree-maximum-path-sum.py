4f652d345710b8dd9e9d17ccb736a9b4c7fe5f11
Juyeong Park <sprnd645@gmail.com>
2024-06-02 01:14:59 +0900

    Time: 4965 ms (5.21%), Space: 20.9 MB (74.23%) - LeetHub

---
+++ LeetCode/Hard/0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -0,0 +1,61 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        queue = deque([root])
+        pathSums = []
+
+        # 모든 노드를 돌면서, 자신이 포함된 경로의 최대 합계를 구한다.
+        while len(queue):
+            queueLen = len(queue)
+            for i in range(queueLen):
+                node = queue.popleft()
+                left = self.maxPathSumFrom(node.left)
+                right = self.maxPathSumFrom(node.right)
+
+                # 현재 노드를 포함한 값 중 경로의 최대값을 추가한다.
+                currentNodeMaxPathSum = max(
+                    node.val,
+                    node.val + left,
+                    node.val + right,
+                    (node.val + left + right)
+                )
+                pathSums.append(currentNodeMaxPathSum)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return max(pathSums)
+    
+    # root를 포함한 경로의 최대값을 리턴한다.
+    def maxPathSumFrom(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        if not root.left and not root.right:
+            return root.val
+        
+        if not root.right:
+            left = self.maxPathSumFrom(root.left)
+            return max(root.val, (root.val + left))
+
+        if not root.left:
+            right = self.maxPathSumFrom(root.right)
+            return max(root.val, (root.val + right))
+        
+        left = self.maxPathSumFrom(root.left)
+        right = self.maxPathSumFrom(root.right)
+
+        return max(
+            root.val,
+            root.val + left,
+            root.val + right,
+        )
+
+    
