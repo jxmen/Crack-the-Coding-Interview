4710b9fc12350d76b201e5ba001f0efe39299f73
Juyeong Park <sprnd645@gmail.com>
2024-06-07 15:54:37 +0900

    Time: 45 ms (14.72%), Space: 16.7 MB (42.42%) - LeetHub

---
+++ LeetCode/Medium/0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,64 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+"""
+중위 순회로 트리를 linked list로 만들기
+
+    1
+   /
+  2
+ / \
+3   4
+
+    1
+   /
+  2
+   \
+    3
+     \
+      4
+
+중위 순회를 잘 이용해보자.
+중위 순회란? - 자신-왼쪽-오른쪽 순으로 순회
+"""
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        
+        if not root:
+            return
+
+        if not root.left and not root.right:
+            return
+
+        self.flatten(root.right)
+        self.flatten(root.left)
+
+        if root.left is not None and root.right is not None:
+            prevRootRight = root.right
+            
+            # 원래 오른쪽이였던 노드를 재배치한 노드 오른쪽에 붙인다.
+            root.right = root.left
+            self.attach(root.right, prevRootRight)
+            root.left = None
+        
+        if root.right is None and root.left is not None:
+            root.right = root.left
+            root.left = None
+        
+        return
+    
+    def attach(self, root: Optional[TreeNode], target: Optional[TreeNode]) -> None:
+        if root.right is None:
+            root.right = target
+            root.left = None
+            return
+
+        self.attach(root.right, target)
