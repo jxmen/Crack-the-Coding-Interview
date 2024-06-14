d91c6a0d0430de2bfa9d03f675da0dfa0f55c05b
Juyeong Park <sprnd645@gmail.com>
2024-06-14 21:29:19 +0900

    Time: 48 ms (7.7%), Space: 16.6 MB (31.93%) - LeetHub

---
+++ LeetCode/Easy/0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+
+        # root1과 root2의 마지막 자식 노드들을 모두 찾고 일치하면 true를 리턴한다.
+        root1Leafs = self.getLeafs(root1)
+        root2Leafs = self.getLeafs(root2)
+        if len(root1Leafs) != len(root2Leafs):
+            return False
+
+        for i in range(len(root1Leafs)):
+            if root1Leafs[i].val != root2Leafs[i].val:
+                return False
+
+        return True
+
+    def getLeafs(self, root: Optional[TreeNode]) -> List[TreeNode]:
+        if not root:
+            return []
+
+        if not root.left and not root.right:
+            return [root]
+
+        leafs = []
+        leafs.extend(self.getLeafs(root.left))
+        leafs.extend(self.getLeafs(root.right))
+
+        return leafs
