2a13fb2fae1f7a4d80d9b6164a9be34175b5587f
Juyeong Park <sprnd645@gmail.com>
2024-06-29 00:49:23 +0900

    Time: 107 ms (76.78%), Space: 17.6 MB (98.73%) - LeetHub

--- LeetCode/Easy/0268-missing-number/0268-missing-number.py
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.py
@@ -1,9 +1,16 @@
 class Solution:
-    def missingNumber2(self, nums: List[int]) -> int:
-        # TODO: Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
-        pass
-
+    
+    # only O(1) extra space complexity and O(n) runtime complexity?
     def missingNumber(self, nums: List[int]) -> int:
+        # 공간을 어떻게 1개만 쓰고 풀 수 있을까? - 합계 공식을 사용하자.
+
+        # n = 10이라면 sum = 45가 되어야 한다.
+        # 만약 8이 빠졌다면 45-8 = 37일 것이다. (n은 하나가 빠졌으므로 +1 해준다.)
+        nNumsSum = sum([i for i in range(len(nums)+1)])
+        return nNumsSum - sum(nums)
+
+    # set을 이용한 풀이 - space O(N), time O(N)
+    def missingNumber2(self, nums: List[int]) -> int:
         numSet = set(nums) # space O(N), time O(N)
         for i in range(len(nums)+1): # time O(N)
             if i not in numSet:
