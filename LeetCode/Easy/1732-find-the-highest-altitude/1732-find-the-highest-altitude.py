2c1a515a9d678ea55385a7f61cea26e1bccb9870
Juyeong Park <sprnd645@gmail.com>
2024-06-29 15:50:52 +0900

    Time: 46 ms (8.81%), Space: 16.5 MB (69.62%) - LeetHub

---
+++ LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,15 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        largest = 0
+        prefixes = [0] # 처음엔 무조건 0으로 시작한다.
+        for i in range(len(gain)):
+            
+            # 구간 합을 추가한다.
+            prefix = prefixes[i] + gain[i]
+            prefixes.append(prefix)
+
+            # 최대값을 갱신한다.
+            largest = max(largest, prefix)
+        
+        return largest
+        
