46ea7f058ea5318f33a9f5c78349295f8d2b7525
Juyeong Park <sprnd645@gmail.com>
2024-06-29 18:25:17 +0900

    Time: 447 ms (8.3%), Space: 54.8 MB (5.02%) - LeetHub

--- LeetCode/Medium/2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ LeetCode/Medium/2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -7,17 +7,15 @@ class ListNode:
 
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        maxTwinSum = 0
-
         # fast는 맨 끝으로 이동한다.
-        slow, fast = head, head
-        length = 0
+        slow, fast, length = head, head, 0
         while fast and fast.next:
             fast.next.prev = fast
             fast = fast.next
             length += 1
         
         # twin sum을 구한다.
+        maxTwinSum = 0
         for i in range(length):
             maxTwinSum = max(maxTwinSum, slow.val + fast.val)
             slow = slow.next
