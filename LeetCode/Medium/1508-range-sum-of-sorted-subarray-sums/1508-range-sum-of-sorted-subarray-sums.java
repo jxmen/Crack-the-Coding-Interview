642c7a5f4f75005bd0fba96a00c944b47c184f99
Juyeong Park <sprnd645@gmail.com>
2024-08-04 13:24:34 +0900

    Time: 203 ms (12.16%), Space: 75.1 MB (5.4%) - LeetHub

--- LeetCode/Medium/1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ LeetCode/Medium/1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 
 class Solution {
-	public int rangeSum(int[] nums, int n, int left, int right) {
+	public int rangeSum2(int[] nums, int n, int left, int right) {
 		List<Long> subArraySums = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
 			long sum = 0;
@@ -21,4 +22,28 @@ class Solution {
 		// 모듈로 연산을 사용하여 결과가 int 범위 내에 들어가도록 합니다.
 		return (int) (result % 1_000_000_007);
 	}
+
+	// 최소 힙으로 결과 최적화 버전
+	public int rangeSum(int[] nums, int n, int left, int right) {
+		PriorityQueue<Long> minHeap = new PriorityQueue<>();
+		for (int i = 0; i < n; i++) {
+			long sum = 0;
+			for (int j = i; j < n; j++) {
+				sum += nums[j];
+				minHeap.offer(sum);
+			}
+		}
+
+		long result = 0;
+		for (int i = 1; i <= right; i++) {
+			Long currentSum = minHeap.poll();
+			if (i >= left) {
+				// 모듈로 연산을 사용하여 결과가 int 범위 내에 들어가도록 합니다.
+				result = (result + currentSum) % 1_000_000_007;
+			}
+		}
+
+		return (int) result;
+	}
+
 }
