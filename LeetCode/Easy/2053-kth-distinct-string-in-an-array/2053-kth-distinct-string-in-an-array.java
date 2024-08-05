d26a6ce19c83ed476003a55dc92a3ff0bbb182e2
Juyeong Park <sprnd645@gmail.com>
2024-08-05 15:04:42 +0900

    Time: 6 ms (91.98%), Space: 43.9 MB (88.77%) - LeetHub

--- LeetCode/Easy/2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
+++ LeetCode/Easy/2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
@@ -1,15 +1,15 @@
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 class Solution {
 	public String kthDistinct(String[] arr, int k) {
-		Map<String, Integer> countMap = new HashMap<>();
-		Arrays.stream(arr).forEach(s -> countMap.put(s, countMap.getOrDefault(s, 0) + 1));
+		Map<String, Integer> linkedHashMap = new LinkedHashMap<>();
+		Arrays.stream(arr).forEach(s -> linkedHashMap.put(s, linkedHashMap.getOrDefault(s, 0) + 1));
 
 		// array를 돌면서 key의 value가 하나인 것 중 k번째 key 리턴
 		for (String s: arr) {
-			if (countMap.get(s) == 1) {
+			if (linkedHashMap.get(s) == 1) {
 				if (k == 1) {
 					return s;
 				}
