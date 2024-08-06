77bdd2e76cadbbaf86b663de8d3006d9f345e3d0
Juyeong Park <sprnd645@gmail.com>
2024-08-06 12:45:02 +0900

    Time: 11 ms (57.75%), Space: 44.5 MB (5.29%) - LeetHub

---
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,138 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+class Solution {
+	public boolean isIsomorphic(String s, String t) {
+		return isIsomorphicWithCounter(s, t);
+	}
+
+	public boolean isIsomorphicWithCounter(String s, String t) {
+		Map<Character, Integer> map1 = new HashMap<>();
+		List<Integer> list1 = new ArrayList<>();
+
+		// paper/title의 경우
+		// p - 0, a - 1, e - 2, r - 3 형태로 매핑이 될 것이고
+		// title은 t - 0, i - 1, l - 2, e - 3 형태로 매핑이 될 것이다.
+		int count = 0;
+		for (char c : s.toCharArray()) {
+			if (!map1.containsKey(c)) {
+				map1.put(c, count++);
+			}
+			list1.add(map1.get(c));
+		}
+
+		count = 0;
+		Map<Character, Integer> map2 = new HashMap<>();
+		List<Integer> list2 = new ArrayList<>();
+		for (char c : t.toCharArray()) {
+			if (!map2.containsKey(c)) {
+				map2.put(c, count++);
+			}
+			list2.add(map2.get(c));
+		}
+
+		// 두 리스트가 같은지 확인한다.
+		return list1.equals(list2);
+	}
+
+	public boolean isIsomorphicWithLinkedList(String s, String t) {
+		// {b:3} -> {a:1} -> {b:1} -> {a:1}
+		List<Map.Entry<Character, Integer>> entries = new LinkedList<>();
+		Character prev = null;
+		int count = 0;
+		for (char c : s.toCharArray()) {
+			if (prev == null) {
+				prev = c;
+				count = 1;
+				continue;
+			}
+
+			if (Objects.equals(c, prev)) {
+				count += 1;
+			} else {
+				entries.add(Map.entry(prev, count));
+				prev = c;
+				count = 1;
+			}
+		}
+
+		if (count != 0) {
+			entries.add(Map.entry(prev, count));
+		}
+
+		prev = null;
+		count = 0;
+		// {b:c} , {a:d} 형태로 문자열을 담는다. 기존에 키가 있는데 value가 다르다면 false를 리턴한다.
+		Map<Character, Character> map = new HashMap<>();
+		Iterator<Map.Entry<Character, Integer>> iterator = entries.iterator();
+		for (char c : t.toCharArray()) {
+			if (prev == null) {
+				prev = c;
+				count = 1;
+				continue;
+			}
+
+			if (Objects.equals(c, prev)) {
+				count += 1;
+			} else {
+				// 카운트가 다르면 바로 false
+				if (!iterator.hasNext()) return false;
+				Map.Entry<Character, Integer> next = iterator.next();
+				Character key = next.getKey();
+				Integer value = next.getValue();
+
+				if (count != value) return false;
+				if (map.containsKey(key) && map.get(key) != prev) return false;
+
+				map.put(key, prev);
+				prev = c;
+				count = 1;
+			}
+		}
+
+		// 마지막에 남은 값이 있는지 확인한다.
+		if (!iterator.hasNext()) return false;
+
+		Map.Entry<Character, Integer> next = iterator.next();
+		Character key = next.getKey();
+		Integer value = next.getValue();
+		if (map.containsKey(key) && map.get(key) != prev) return false;
+		return value == count;
+	}
+
+	// 실패
+	public boolean isIsomorphicWithHashMap(String s, String t) {
+		// a -> b로 대체될 수 있지만 동일한 문자에 대해 다른 문자로 대체될 수 없다.
+		// aaa -> aaa는 어떤 케이스인가? a가 a로 변경되었다고 가정해야 하나?
+		// hashmap을 2개 만들고, 두개의 value가 모두 동일하다면 true를 리턴한다.
+		Map<String, Integer> map1 = new HashMap<>(); // LinkedHashMap??
+		Map<String, Integer> map2 = new HashMap<>();
+		Arrays.stream(s.split("")).forEach(c -> map1.put(c, map1.getOrDefault(c, 0) + 1));
+		Arrays.stream(t.split("")).forEach(c -> map2.put(c, map2.getOrDefault(c, 0) + 1));
+
+		List<Integer> values = map1.values().stream().sorted().collect(Collectors.toList());
+		List<Integer> values2 = map2.values().stream().sorted().collect(Collectors.toList());
+
+		//  bba/bab
+		// bbbaba / abbbba
+		// b:4, a:2
+
+		for (int i = 0; i < values.size(); i++) {
+			Integer i1 = values.get(i);
+			Integer i2 = values2.get(i);
+			if (i1 != i2) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
