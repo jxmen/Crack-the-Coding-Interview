62db01b7123696146a11be8ed095f16827d03edb
Juyeong Park <me@jxmen.dev>
2024-09-19 16:30:08 +0900

    [LeetCode/Medium] 198 | 34 ms(71.14%), 16.4 MB(68.73%)
     https://leetcode.com/problems/house-robber

--- LeetCode/Medium/0198-house-robber/0198-house-robber.py
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.py
@@ -1,7 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <= 2:
-            return max(nums)
+        if len(nums) == 1:
+            return nums[0]
 
         n = len(nums)
         dp = [0] * n
