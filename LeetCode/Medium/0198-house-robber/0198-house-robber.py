d24d638c842e4bcdcb74d55af737f37a78d3f93f
Juyeong Park <me@jxmen.dev>
2024-09-19 16:29:31 +0900

    [LeetCode/Medium] 198 | 31 ms(84.73%), 16.4 MB(68.73%)
     https://leetcode.com/problems/house-robber

---
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return max(nums)
+
+        n = len(nums)
+        dp = [0] * n
+  
+        """
+        맨 뒤에서부터 계산하자 (자기 자신의 값이 확정된 값이다!)
+        [10, 2, 3, 100, 4]
+        dp[n-1] = nums[n-1] = 4
+        dp[n-2] = num[n-2] = 100
+        dp[n-3] = nums[n-3] + max(dp[n-3+2:])
+        dp[n-4] = nums[n-4] + max(dp[n-4+2:])
+        """
+
+        # dp[n-1], dp[n-2] 세팅
+        dp[n-1] = nums[n-1]
+        dp[n-2] = nums[n-2]
+
+        # dp 계산
+        for i in range(n-3, -1, -1):
+            dp[i] = nums[i] + max(dp[i+2:])
+        
+        return max(dp)
