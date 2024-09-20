bb869be58e388227dc5b78d685bc909d0889742d
jxmen <sprnd645@gmail.com>
2024-09-20 16:52:50 +0900

    전화번호 목록 문제 dict 대신 set으로 변경

--- Programmers/2/42577. 전화번호 목록/전화번호 목록.py
+++ Programmers/2/42577. 전화번호 목록/전화번호 목록.py
@@ -2,15 +2,12 @@ def solution(phone_book):
     if len(phone_book) == 1:
         return False
 
-    phone_dict = {}
-    for phone in phone_book:
-        phone_dict[phone] = 1
-
+    phones = set(phone_book)
     for phone in phone_book:
         prefix = ""
         for char in phone:
             prefix += char
-            if prefix in phone_dict and prefix != phone:
+            if prefix in phones and prefix != phone:
                 return False
 
     return True
