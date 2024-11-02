7608c7fe036daa2bc2608be1cc714e1b682d957f
Juyeong Park <me@jxmen.dev>
2024-11-02 23:54:34 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Programmers/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,11 @@
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse=True)
+    apple_box = []
+    for i in range(0, len(score), m):
+        apple_box.append(score[i:i+m])
+    for apple in apple_box:
+        if len(apple) == m:
+            answer += min(apple) * m
+
+    return answer
