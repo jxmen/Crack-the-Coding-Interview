ca41f60df78faca4ea69fd1e60e282e65e41e1ca
Juyeong Park <me@jxmen.dev>
2024-11-06 23:44:21 +0900

    [LeetCode/Medium] 200 | 250 ms(46.85%), 19.3 MB(44.69%)
     https://leetcode.com/problems/number-of-islands

---
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        dx = [1, 0, -1, 0]
+        dy = [0, 1, 0, -1]
+        visited = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
+
+        def visit(x, y):
+            nonlocal count
+            nonlocal visited
+
+            visited[x][y] = 1
+
+            # 상하좌우로 이동하면서 반복한다.
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]
+                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == "1" and not visited[nx][ny]:
+                    visit(nx, ny)
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1" and not visited[i][j]:
+                    visit(i, j)
+                    count += 1
+
+        return count
