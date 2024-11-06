2fb9fd345eb4783b3d66dd3123aab01b22aaecc3
Juyeong Park <me@jxmen.dev>
2024-11-06 23:46:02 +0900

    [LeetCode/Medium] 200 | 244 ms(54.41%), 18.9 MB(88.99%)
     https://leetcode.com/problems/number-of-islands

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
@@ -5,22 +5,19 @@ class Solution:
         dy = [0, 1, 0, -1]
         visited = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
 
-        def visit(x, y):
-            nonlocal count
-            nonlocal visited
-
+        def visit(x, y, visited):
             visited[x][y] = 1
 
             # 상하좌우로 이동하면서 반복한다.
             for i in range(4):
                 nx, ny = x + dx[i], y + dy[i]
                 if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == "1" and not visited[nx][ny]:
-                    visit(nx, ny)
+                    visit(nx, ny, visited)
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1" and not visited[i][j]:
-                    visit(i, j)
+                    visit(i, j, visited)
                     count += 1
 
         return count
