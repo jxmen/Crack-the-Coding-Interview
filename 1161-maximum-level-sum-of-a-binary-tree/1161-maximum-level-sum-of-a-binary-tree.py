96a34fb8960668bcab8e2c1fc5c49e4426fb9c6c
Juyeong Park <sprnd645@gmail.com>
2026-01-06 15:57:17 +0900

    Time: 16 ms (77.56%), Space: 23.1 MB (13.07%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -11,21 +11,53 @@ class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
         sums = []
         
+        # bfs로 각 레벨별 합계를 최대값과 비교하고 갱신
+        dq = deque([root])
+        current_level = 1
+        max_sum, max_level = root.val, 1
+        while dq:
+            dq_len = len(dq)
+            current_level_sum = 0
+            for _ in range(dq_len):
+                node = dq.popleft()
+                current_level_sum += node.val
+
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+            
+            # 최대값 갱신
+            if current_level_sum > max_sum:
+                max_sum = current_level_sum
+                max_level = current_level
+            
+            current_level += 1
+        
+        return max_level
+
+
+    def maxLevelSum2(self, root: Optional[TreeNode]) -> int:
+        sums = []
+        
         # bfs로 각 레벨별 합계 구하기
         dq = deque([root])
         while dq:
             dq_len = len(dq)
-            level_sum = 0
+            current_level_sum = 0
             for _ in range(dq_len):
-                popped = dq.popleft()
-                level_sum += popped.val
+                node = dq.popleft()
+                current_level_sum += node.val
 
-                if popped.left:
-                    dq.append(popped.left)
-                if popped.right:
-                    dq.append(popped.right)
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
             
-            sums.append(level_sum)
+            sums.append(current_level_sum)
 
         # 최대 레벨 값 구하기
         return sums.index(max(sums)) + 1
+
+
+   
