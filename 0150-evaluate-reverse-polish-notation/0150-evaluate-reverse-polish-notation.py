7196b2bd328f329e5a73d8b0c0cec94f90e85708
Juyeong Park <sprnd645@gmail.com>
2026-01-08 15:10:09 +0900

    Time: 3 ms (66.45%), Space: 20.9 MB (5.15%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,24 +1,42 @@
+import operator # 파이썬 표준 라이브러리
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        
-        # 토큰을 다 돌린다. 연산자를 만나면 스택에서 숫자 2개를 연산한 값을 다시 넣는다. 스택에 맨 마지막에 남은 하나의 값을 리턴한다.
-
-        operations = set(['+', '-', '*', '/'])
         stack = []
+        operation_map = {
+            '+': operator.add, # lambda a,b와 같음
+            '-': operator.sub,
+            '*': operator.mul,
+            '/': lambda a, b: int(a / b), # truncates toward zero
+        }
+
         for token in tokens:
-            if token not in operations:
-                stack.append(token)
-            elif token == '+':
-                num2, num1 = int(stack.pop()), int(stack.pop())
-                stack.append(num1 + num2)
-            elif token == '-':
-                num2, num1 = int(stack.pop()), int(stack.pop())
-                stack.append(num1 - num2)
-            elif token == '*':
-                num2, num1 = int(stack.pop()), int(stack.pop())
-                stack.append(num1 * num2)
-            elif token == '/':
-                num2, num1 = int(stack.pop()), int(stack.pop())
-                stack.append(int(num1 / num2))
+            if token not in operation_map:
+                stack.append(int(token))
+                continue
+
+            num2, num1 = stack.pop(), stack.pop()
+            stack.append(operation_map[token](num1, num2))
         
-        return int(stack[0])
+        return stack[0]
+
+
+    def evalRPN_with_lambda(self, tokens: List[str]) -> int:
+        # 토큰을 다 돌린다. 연산자를 만나면 스택에서 숫자 2개를 연산한 값을 다시 넣는다. 스택에 맨 마지막에 남은 하나의 값을 리턴한다.
+        stack = []
+        operation_map = {
+            '+': lambda a, b: a + b,
+            '-': lambda a, b: a - b,
+            '*': lambda a, b: a * b,
+            '/': lambda a, b: int(a / b), # truncates toward zero
+        }
+        
+        for token in tokens:
+            if token not in operation_map:
+                stack.append(int(token))
+                continue
+
+            num2, num1 = stack.pop(), stack.pop()
+            stack.append(operation_map[token](num1, num2))
+        
+        return stack[0]
