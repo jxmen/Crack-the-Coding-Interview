b3aa0bc933bd68d427dc98bf7128782e54e73fc3
Juyeong Park <sprnd645@gmail.com>
2026-01-08 14:11:50 +0900

    Time: 0 ms (100%), Space: 19.4 MB (9.65%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -3,18 +3,14 @@ class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         operations = []
         num = 1
-        nums = []
         target_pointer = 0
         
-        while num <= n and target_pointer < len(target):
-            # num 일단 nums에 넣고, pointer값과 다르다면 pop 한다.
+        while target_pointer < len(target):
             operations.append("Push")
-            nums.append(num)
 
             # pointer값과 다르면 pop
             if num != target[target_pointer]:
                 operations.append("Pop")
-                nums.pop()
             else:
                 target_pointer += 1
             
