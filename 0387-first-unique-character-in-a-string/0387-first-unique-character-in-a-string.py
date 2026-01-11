cb91b32628f02453241416c3417ad02de083c3ce
Juyeong Park <sprnd645@gmail.com>
2026-01-12 00:14:49 +0900

    Time: 167 ms (5.06%), Space: 22.4 MB (6.6%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,5 +1,23 @@
+from collections import deque
+
 class Solution:
+
+    # 큐로 구현하기
     def firstUniqChar(self, s: str) -> int:
+        count = {}
+        for c in s:
+            count[c] = count.get(c, 0) + 1
+
+        dq = deque()
+        for i, c in enumerate(s):
+            dq.append((c, i)) # 인덱스도 저장
+            
+            while dq and count[dq[0][0]] > 1:
+                dq.popleft()
+            
+        return dq[0][1] if dq else -1
+
+    def firstUniqChar2(self, s: str) -> int:
         # 큐를 안써도 되지 않나?
         count = {}
         for c in s:
