0bfa41893c07dcc90e719b12e1b77c4103e52a2f
Juyeong Park <sprnd645@gmail.com>
2026-01-12 00:09:03 +0900

    Time: 75 ms (30.09%), Space: 19.8 MB (6.6%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,15 +1,12 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         # 큐를 안써도 되지 않나?
-        d = {}
+        count = {}
         for c in s:
-            if c not in d:
-                d[c] = 1
-            else:
-                d[c] += 1
+            count[c] = count.get(c, 0) + 1
         
         for i, c in enumerate(s):
-            if d[c] == 1:
+            if count[c] == 1:
                 return i
         
         return -1
