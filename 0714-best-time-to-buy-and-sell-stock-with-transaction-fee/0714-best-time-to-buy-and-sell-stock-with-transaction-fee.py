9155a5e07625dad47ae9de2f88b07dfed889274d
Juyeong Park <sprnd645@gmail.com>
2026-01-18 19:13:09 +0900

    Time: 96 ms (73.51%), Space: 26.2 MB (44.34%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,6 +2,7 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
 
+        # 주식을 안 가질 때 / 가질때 최대 이익 값 저장하는 배열
         non = [0] * n
         take = [0] * n
 
@@ -9,7 +10,7 @@ class Solution:
         take[0] = -prices[0] # 주식을 '샀기' 때문에, 최대 이익은 -로 시작한다.
 
         for i in range(1, n):
-            # 판다 = (팔기전 금액 + 지금 금액 - 수수료)
+            # 판다 = (팔기전 최대이익 + 지금 금액 - 수수료)
             selling = take[i-1] + prices[i] - fee
 
             # 산다 = (안샀을때 최대이익 - 지금가격)
@@ -19,4 +20,3 @@ class Solution:
             take[i] = max(buying, take[i-1]) # 살때, 안팔때
 
         return max(non[n-1], take[n-1])
-        
