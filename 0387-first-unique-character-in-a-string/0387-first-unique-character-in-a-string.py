8f7529d4f5c8c1dc8f9af508e60dd14196661a86
Juyeong Park <sprnd645@gmail.com>
2026-02-01 14:25:12 +0900

    Time: 47 ms (87.58%), Space: 19.9 MB (14.62%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,30 +1,9 @@
-from collections import deque
+from collections import Counter
 
 class Solution:
-
-    # 큐로 구현하기
     def firstUniqChar(self, s: str) -> int:
-        count = {}
-        for c in s:
-            count[c] = count.get(c, 0) + 1
-
-        dq = deque()
+        counter = Counter(s)
         for i, c in enumerate(s):
-            dq.append((c, i)) # 인덱스도 저장
-            
-            while dq and count[dq[0][0]] > 1:
-                dq.popleft()
-            
-        return dq[0][1] if dq else -1
-
-    def firstUniqChar2(self, s: str) -> int:
-        # 큐를 안써도 되지 않나?
-        count = {}
-        for c in s:
-            count[c] = count.get(c, 0) + 1
-        
-        for i, c in enumerate(s):
-            if count[c] == 1:
+            if counter[c] == 1:
                 return i
-        
         return -1
