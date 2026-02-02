1c602a671b280e8bdea45ca28943a4df762ce3bd
Juyeong Park <sprnd645@gmail.com>
2026-02-02 13:54:08 +0900

    Time: 0 ms (100%), Space: 20.7 MB (14.69%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,29 +5,24 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # stack을 쓸까?
-        # 스택에 다 넣고 마지막꺼를 pop해서 연결하자
+        # 재귀로 구현하기
+        if not head or not head.next:
+            return head
 
-        stack = []
+        new_head = self.reverseList(head.next)
+
+        head.next.next = head
+        head.next = None
+
+        return new_head
+
+    def reverseList2(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
         node = head
         while node:
-            stack.append(node)
-            node = node.next
-        
-        # 스택에서 하나를 꺼내고, 스택 마지막에 노드가 있다면 next로 연결한다.
-        if not stack:
-            return node
-        
-        node = stack[-1]
-        while stack:
-            last = stack.pop()
-            if stack:
-                last.next = stack[-1]
-            else:
-                last.next = None
-        
-        if last:
-            last.next = None
-        
-        return node
-        
+            temp = node.next
+            node.next = prev
+            prev = node
+            node = temp
+
+        return prev
