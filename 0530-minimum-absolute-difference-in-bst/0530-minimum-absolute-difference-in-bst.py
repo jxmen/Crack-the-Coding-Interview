1dc4f933db815d0b5b5bb0ade4ca1ce9c38ea89e
Juyeong Park <sprnd645@gmail.com>
2026-02-02 23:30:29 +0900

    Time: 7 ms (25.42%), Space: 20.9 MB (29.27%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -21,7 +21,13 @@ class Solution:
 
             return v
 
-        values = get_values(root)
+        # get_values를 깔끔하게 바꾼 버전
+        def inorder(node):
+            if not node:
+                return []
+            return inorder(node.left) + [node.val] + inorder(node.right)
+
+        values = inorder(root)
         min_diff = float('inf')
         for i in range(1, len(values)):
             min_diff = min(min_diff, values[i] - values[i-1])
