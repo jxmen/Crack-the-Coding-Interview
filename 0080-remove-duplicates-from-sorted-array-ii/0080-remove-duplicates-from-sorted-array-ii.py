2d01d60932f15332eee172245529a3819e31d357
Juyeong Park <sprnd645@gmail.com>
2026-02-03 14:39:54 +0900

    Time: 86 ms (52.72%), Space: 21.9 MB (42.36%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,6 +1,23 @@
 class Solution:
+
+    # 정석 풀이
     def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return len(nums)
+        
+        k = 2 # 중복된 값을 덮어씌울 위치
+        for i in range(2, len(nums)):
+            
+            # 중복을 최대 2개까지만 허용하므로, k-2 값이 다를 경우에만 덮어씌운다.
+            if nums[i] != nums[k - 2]:
+                nums[k] = nums[i] # 덮어씌우기 
+                k += 1 # 덮어씌울 위치 이동
+        
+        return k
+
+    def removeDuplicates2(self, nums: List[int]) -> int:
         n = len(nums)
+
         p1, p2 = 0, 1
         # 1. 중복되는게 3개 이상 있다면, None으로 바꾼다.
         # [1,1,1,2] -> [1,1,_,2]
