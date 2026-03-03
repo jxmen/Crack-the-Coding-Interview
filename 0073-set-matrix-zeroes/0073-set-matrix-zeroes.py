413de5a9aba778dc26b7489bf85d565186bf8bcf
Juyeong Park <sprnd645@gmail.com>
2026-03-03 16:36:34 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,6 +1,24 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        
+        zero_rows = set()
+        zero_cols = set()
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    zero_rows.add(i)
+                    zero_cols.add(j)
+
+        for r in zero_rows:
+            for j in range(len(matrix[0])):
+                matrix[r][j] = 0
+
+        for c in zero_cols:
+            for i in range(len(matrix)):
+                matrix[i][c] = 0
+
+
+    def setZeroes2(self, matrix: List[List[int]]) -> None:
+
         # matrix에서 0인 좌표들을 찾고, 해당 위치를 기억하여 row와 column을 모두 0으로 만든다.
 
         directions = []
@@ -8,12 +26,11 @@ class Solution:
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
                     directions.append((i, j))
-        
+
         for d in directions:
             x, y = d
             for i in range(len(matrix)):
                 matrix[i][y] = 0
-            
+
             for j in range(len(matrix[0])):
                 matrix[x][j] = 0
-        
