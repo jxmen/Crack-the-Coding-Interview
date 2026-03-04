fe2757e45444d31ef35af8ef70925506754dc3eb
Juyeong Park <sprnd645@gmail.com>
2026-03-04 14:19:30 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,34 +1,25 @@
-"""
-아나그램 판별을 어떻게 하지? map?
-
-1. 서로 일치한지 비교를 어떻게 할 것인가?
-2. 인덱스가 늘어날 때 map을 어떻게 업데이트 할 것인가?
-
-"""
+from collections import Counter
 
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         n = len(p)
         ans = []
-
-        p_freq = {}
-        for c in p:
-            p_freq[c] = p_freq.get(c, 0) + 1
-
-        s_freq = {}
-        for c in s[:n]:
-            s_freq[c] = s_freq.get(c, 0) + 1
+        p_freq = Counter(p)
+        s_freq = Counter(s[:n])
 
         if p_freq == s_freq:
             ans.append(0)
 
         for i in range(1, len(s) - n + 1):
-            s_freq[s[i - 1]] = s_freq.get(s[i - 1], 0) - 1
-            if s_freq.get(s[i - 1]) == 0:
-                del s_freq[s[i - 1]]
+            left, right = s[i - 1], s[i + n - 1]
 
-            s_freq[s[i + n - 1]] = s_freq.get(s[i + n - 1], 0) + 1
+            s_freq[left] -= 1
+            if s_freq[left] == 0:
+                del s_freq[left]
+
+            s_freq[right] += 1
+
             if p_freq == s_freq:
                 ans.append(i)
 
