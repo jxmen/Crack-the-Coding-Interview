43080643952d404f8326edb5469a6d7f4467e658
Juyeong Park <sprnd645@gmail.com>
2026-03-19 14:46:36 +0900

    [Gold V] Title: 토마토, Time: 988 ms, Memory: 100460 KB -BaekjoonHub

--- Baekjoon/Gold/7576. 토마토/토마토.py
+++ Baekjoon/Gold/7576. 토마토/토마토.py
@@ -3,21 +3,19 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(m)]
-
-DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
 # 모두 0이라면 level을, 아니라면 -1 리턴
-def solution(grid, n, m):
+def solution(grid, rows, cols):
     queue = deque()
-    for i in range(m):
-        for j in range(n):
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
                 queue.append((i, j))
 
-    level = 0
+    DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    days = 0
+
     while queue:
         len_queue = len(queue)
         for _ in range(len_queue):
@@ -25,19 +23,22 @@ def solution(grid, n, m):
 
             for dx, dy in DIRECTIONS:
                 nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 0:
+                if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == 0:
                     grid[nx][ny] = 1  # 방문 표시
                     queue.append((nx, ny))
 
-        if queue:
-            level += 1
+        days += 1
 
-    for i in range(m):
-        for j in range(n):
+    days = max(0, days - 1)
+
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 0:
                 return -1
 
-    return level
+    return days
 
+cols, rows = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(rows)]
 
-print(solution(grid, n, m))
+print(solution(grid, rows, cols))
