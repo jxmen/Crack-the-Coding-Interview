0ed38f1fcec9db728a453e0846efe485d98b8fbc
Juyeong Park <sprnd645@gmail.com>
2026-03-21 21:01:41 +0900

    [Gold IV] Title: 연구소, Time: 1252 ms, Memory: 33432 KB -BaekjoonHub

--- Baekjoon/Gold/14502. 연구소/연구소.py
+++ Baekjoon/Gold/14502. 연구소/연구소.py
@@ -4,35 +4,24 @@ from itertools import combinations
 
 input = sys.stdin.readline
 
-def solution(grid, combo):
+def solution(grid, combo, viruses, rows, cols):
     def visit(x, y):
         grid[x][y] = 2
         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             nx, ny = x + dx, y + dy
 
-            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == 0:
+            if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == 0:
                 visit(nx, ny)
-
-    def get_zero_count():
-        zero_count = 0
-        for row in grid:
-            for cell in row:
-                if cell == 0:
-                    zero_count += 1
-
-        return zero_count
 
     grid = [row[:] for row in grid]
     for x, y in combo:
         grid[x][y] = 1
 
-    # 전염 로직
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 2:
-                visit(i, j)
+    # 전염 로직 (bfs)
+    for i, j in viruses:
+        visit(i, j)
 
-    return get_zero_count()
+    return sum(cell == 0 for row in grid for cell in row)
 
 
 # 세로 크기 N, 가로 크기 M
@@ -41,14 +30,16 @@ grid = [
     list(map(int, input().split())) for _ in range(rows)
 ]
 
-zeros = []
+zeros, viruses = [], []
 for i in range(rows):
     for j in range(cols):
         if grid[i][j] == 0:
             zeros.append((i, j))
+        elif grid[i][j] == 2:
+            viruses.append((i, j))
 
 max_count = 0
 for combo in combinations(zeros, 3):
-    max_count = max(max_count, solution(grid, combo))
+    max_count = max(max_count, solution(grid, combo, viruses, rows, cols))
 
 print(max_count)
