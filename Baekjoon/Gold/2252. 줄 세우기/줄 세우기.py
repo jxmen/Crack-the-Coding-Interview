95e92707dcfa08c129c97e2542dc0e6b981d43f1
Juyeong Park <sprnd645@gmail.com>
2026-03-22 00:51:26 +0900

    [Gold III] Title: 줄 세우기, Time: 184 ms, Memory: 41472 KB -BaekjoonHub

---
+++ Baekjoon/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,33 @@
+import sys
+
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+in_degree = [0 for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    in_degree[b] += 1
+
+queue = deque()
+for i in range(1, n + 1):
+    if in_degree[i] == 0:
+        queue.append(i)
+
+
+answer = []
+while queue:
+    v = queue.pop()
+    answer.append(v)
+
+    for next_v in graph[v]:
+        in_degree[next_v] -= 1
+        if in_degree[next_v] == 0:
+            queue.append(next_v)
+
+print(' '.join(map(str, answer)))
