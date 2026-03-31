e75518834757439636c446434dffcb4216a3cf5b
Juyeong Park <sprnd645@gmail.com>
2026-03-31 15:45:30 +0900

    [Gold V] Title: 회의실 배정, Time: 236 ms, Memory: 54216 KB -BaekjoonHub

--- Baekjoon/Gold/1931. 회의실 배정/회의실 배정.py
+++ Baekjoon/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,26 +1,31 @@
 import sys
-import heapq
 
 input = sys.stdin.readline
 
 
 def solution(n, arr):
     ans = 0
+    """
+    (끝나는 시간, 시작 시간) 기준으로 정렬한다.
+    
+    끝나는 시간이 같게 주어진다면 시작 시간이 짧은 것이 우선순위로 주어진다.
+    """
+    arr.sort(key=lambda x: (x[1], x[0]))
 
-    heap = [(end, start) for start, end in arr]
-    heapq.heapify(heap)
-
-    while heap:
-        end, start = heapq.heappop(heap)
-        while heap and end > heap[0][1]:
-            heapq.heappop(heap)
-
-        ans += 1
+    """
+    다음 끝나는 시간에 일치하는 회의를 찾을때 까지 탐욕적으로 탐색한다.
+    """
+    last_end = 0
+    for start, end in arr:
+        if start >= last_end:
+            ans += 1
+            last_end = end
 
     return ans
 
 
-n = int(input())
-arr = [tuple(map(int, input().split())) for _ in range(n)]
+if __name__ == '__main__':
+    n = int(input())
+    arr = [tuple(map(int, input().split())) for _ in range(n)]
 
-print(solution(n, arr))
+    print(solution(n, arr))
