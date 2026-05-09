b6c08490e9bf38b2394a4ba1858da7e6e25ac367
Juyeong Park <sprnd645@gmail.com>
2026-05-09 19:08:14 +0900

    Time: 4 ms (42.27%), Space: 52.4 MB (47.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,15 +1,10 @@
 class Solution {
-
+    static int[] dx = new int[]{0, 1, -1, 0};
+    static int[] dy = new int[]{1, 0, 0, -1};
 
     public int numIslands(char[][] grid) {
         int n = grid.length, m = grid[0].length;
-
         boolean[][] visited = new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                visited[i][j] = false;
-            }
-        }
 
         int count = 0;
         for (int i = 0; i < n; i++) {
@@ -24,19 +19,20 @@ class Solution {
         return count;
     }
 
-    private void visit(char[][] grid, boolean[][] visited, int i, int j) {
-        int n = visited.length, m = visited[0].length;
-        visited[i][j] = true;
-        
-        // TODO: 상하좌우 모두 영역을 넘지 않고 1이라면, 또 visit 처리
-        int[] dx = {1, 0, 0, -1};
-        int[] dy = {0, 1, -1, 0};
+    private void visit(char[][] grid, boolean[][] visited, int x, int y) {
+        int n = grid.length, m = grid[0].length;
+        visited[x][y] = true;
 
-        for (int k = 0; k < 4; k++) {
-            int nx = dx[k] + i;
-            int ny = dy[k] + j;
+        for (int i = 0; i < 4; i++) {
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
             
-            if ((0 <= nx && nx < n) && (0 <= ny && ny < m) && !visited[nx][ny] && grid[nx][ny] == '1') {
+            if (
+                (0 <= nx && nx < n) 
+                && (0 <= ny && ny < m) 
+                && !visited[nx][ny] 
+                && grid[nx][ny] == '1'
+            ) {
                 visit(grid, visited, nx, ny);
             }
         }
