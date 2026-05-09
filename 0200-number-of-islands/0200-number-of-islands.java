a68f08d88ff8bedbb3dad9c7797698f94054c009
Juyeong Park <sprnd645@gmail.com>
2026-05-09 19:12:26 +0900

    Time: 4 ms (42.27%), Space: 52.3 MB (60.58%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,6 +1,5 @@
 class Solution {
-    static int[] dx = new int[]{0, 1, -1, 0};
-    static int[] dy = new int[]{1, 0, 0, -1};
+    static int[][] directions = {{1,0}, {0,1}, {-1,0}, {0,-1}};
 
     public int numIslands(char[][] grid) {
         int n = grid.length, m = grid[0].length;
@@ -10,7 +9,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1' && !visited[i][j]) {
-                    visit(grid, visited, i, j);
+                    visit(grid, visited, i, j, n, m);
                     count++;
                 }
             }
@@ -19,21 +18,16 @@ class Solution {
         return count;
     }
 
-    private void visit(char[][] grid, boolean[][] visited, int x, int y) {
-        int n = grid.length, m = grid[0].length;
+    private void visit(char[][] grid, boolean[][] visited, int x, int y, int n, int m) {
         visited[x][y] = true;
 
-        for (int i = 0; i < 4; i++) {
-            int nx = dx[i] + x;
-            int ny = dy[i] + y;
-            
+        for (int[] d: directions) {
+            int nx = d[0] + x, ny = d[1] + y;
             if (
-                (0 <= nx && nx < n) 
-                && (0 <= ny && ny < m) 
-                && !visited[nx][ny] 
-                && grid[nx][ny] == '1'
+                (0 <= nx && nx < n) && (0 <= ny && ny < m) 
+                && !visited[nx][ny] && grid[nx][ny] == '1'
             ) {
-                visit(grid, visited, nx, ny);
+                visit(grid, visited, nx, ny, n, m);
             }
         }
     }
